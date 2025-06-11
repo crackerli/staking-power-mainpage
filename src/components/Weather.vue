3068a59e728c2b7e285e7c37bdd95e58da695b18
crackerli <crackerli@163.com>
2025-06-11 20:57:36 +0800

    update site

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -13,7 +13,7 @@
     <span class="sm-hidden">{{ weatherData.weather.windpower }}&nbsp;级</span>
   </div>
   <div class="weather" v-else>
-    <span>Staking Day</span>
+    <span>Happy nice day</span>
   </div>
 </template>
 
