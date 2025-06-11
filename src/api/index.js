2c9ec6b03f0e8c716d9a8f27dd25ee3000504493
crackerli <crackerli@163.com>
2025-06-11 23:57:20 +0800

    update site

--- src/api/index.js
+++ src/api/index.js
@@ -55,13 +55,14 @@ hitokotoList = [
 // 获取一言数据
 export const getHitokoto = async () => {
 //  const res = await fetch("https://v1.hitokoto.cn");
-  const res = hitokotoList[0];
   const hitokotoList = [
     {
       "hitokoto": "\"staking-power.poolv1.near\" is the one of the top 100 validators",
       "from": "Delegate, take profit"
     },
   ];
+  const res = hitokotoList[0];
+
 //  return await res.json();
   return await res;
 };
