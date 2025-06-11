503650a33ca622b141e5ae7549e88a284d019761
crackerli <crackerli@163.com>
2025-06-11 20:45:14 +0800

    update site

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -28,10 +28,10 @@ export const getCurrentTime = () => {
 export const getTimeCapsule = () => {
   const now = dayjs();
   const dayText = {
-    day: "今日",
-    week: "本周",
-    month: "本月",
-    year: "本年",
+    day: "Today",
+    week: "This week",
+    month: "This year",
+    year: "This year",
   };
   /**
    * 计算时间差的函数
@@ -70,25 +70,6 @@ export const getTimeCapsule = () => {
 
 // 欢迎提示
 export const helloInit = () => {
-  const hour = new Date().getHours();
-  let hello = null;
-  if (hour < 6) {
-    hello = "凌晨好";
-  } else if (hour < 9) {
-    hello = "早上好";
-  } else if (hour < 12) {
-    hello = "上午好";
-  } else if (hour < 14) {
-    hello = "中午好";
-  } else if (hour < 17) {
-    hello = "下午好";
-  } else if (hour < 19) {
-    hello = "傍晚好";
-  } else if (hour < 22) {
-    hello = "晚上好";
-  } else {
-    hello = "夜深了";
-  }
   ElMessage({
     dangerouslyUseHTMLString: true,
     message: `Welcome to Staking-Power Service`,
@@ -109,13 +90,12 @@ export const checkDays = () => {
   const date = myDate.getDate();
   const key = `${mon}.${date}`;
   if (Object.prototype.hasOwnProperty.call(anniversaries, key)) {
-    console.log(`今天是${anniversaries[key]}`);
     const gray = document.createElement("style");
     gray.innerHTML = "html{filter: grayscale(100%)}";
     document.head.appendChild(gray);
     ElMessage({
-      message: `今天是${anniversaries[key]}`,
-      duration: 14000,
+      message: `Todays is ${anniversaries[key]}`,
+      duration: 0,
       icon: h(SpaCandle, { theme: "filled", fill: "#efefef" }),
     });
   }
@@ -140,5 +120,5 @@ export const siteDateStatistics = (startDate) => {
     months += 12;
   }
 
-  return `本站已经苟活了 ${years} 年 ${months} 月 ${days} 天`;
+  return `This site has been ${years} years ${months} months ${days} days`;
 };
